7302c1d73abd17a97ec07f7760fedbd7470a57e8
CooganKoerts <coogsk@gmail.com>
2020-02-04 20:08:42 -0500

    Initial Commit

---
+++ SpringBoot-Todo/src/main/java/com/coogan/angular/rest/webservices/angularrestfulwebservices/AngularRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.coogan.angular.rest.webservices.angularrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Todo/src/test/java/com/coogan/angular/rest/webservices/angularrestfulwebservices/AngularRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coogan.angular.rest.webservices.angularrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
